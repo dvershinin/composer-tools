55b7e877cac30143fdde0a4cb1c167e4679c8426
Danila Vershinin <ciapnz@gmail.com>
2015-07-15 18:33:41 +0400

    Update

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 cd "$( dirname "${BASH_SOURCE[0]}")"
 composer update
 
-sudo ln -s /usr/local/src/composer-tools/vendor/bin/phpunit /usr/local/bin/
+sudo ln -sf /usr/local/src/composer-tools/vendor/bin/phpunit /usr/local/bin/
